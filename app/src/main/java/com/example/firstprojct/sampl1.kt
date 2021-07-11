1bf53799036246077815b0081058c4f04b6f85be
Yesthink <kingcarnam@gmail.com>
2021-07-11 21:29:43 +0900

    kotlin Funtion Practice

---
+++ app/src/main/java/com/example/firstprojct/sampl1.kt
@@ -0,0 +1,57 @@
+package com.example.firstprojct
+
+fun main(){
+    helloWorld()
+
+    println(add(1,2))
+
+
+    stringTem("hyungjin")
+}
+
+
+/*void : Unit*/
+fun helloWorld(){
+    println("hello kotlin world")
+}
+
+/* clarify argument and return*/
+fun add(a : Int, b : Int) : Int{
+    return a+b
+}
+
+/* val vs var */
+fun hi(){
+    val a  = 10
+
+    var b  = 9
+
+
+//    a = 100
+    b = 10
+    val c = 11
+    var d = 12
+    var name = "jin"
+
+    println("$a$b$c$d$name")
+}
+
+/* String Template */
+fun stringTem(str : String) {
+    println("my name is ${str}desu")
+    println("is this true? ${1==2}")
+    println("is this true? 2\$a")
+}
+
+
+/*cas state*/
+fun maxBy(a : Int, b : Int) : Int {
+
+    if( a>b ){
+        return a
+    } else{
+        return b
+    }
+}
+
+fun maxBy2(a : Int, b: Int) = if(a>b) a else b
